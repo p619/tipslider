4e8b07ae71ef6d61d9c364329553bc4fbccfb9ab
p619 <p.619@hotmail.com>
2016-11-17 20:55:49 +0800

    Initial Commit

---
+++ 1117小費器/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  1117小費器
+//
+//  Created by heroshi on 2016/11/17.
+//  Copyright © 2016年 heroshi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
